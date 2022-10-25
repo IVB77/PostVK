383b54899954279a6ddaa5d6c95560f63b4c52c0
Vladimir Ivshin <ivshinvb@mail.ru>
2022-10-25 20:58:39 +0500

    Add new parameter Attachment

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,9 +9,84 @@ data class Post(
     val friendsOnly: Boolean,
     val views: Views,
     val copyHistory: Array<Int>?,
+    val attachments: Array<Attachment>?,
     var id: Int = 0
-) {
+)
 
+interface Attachment {
+    val type: String
+}
+
+class VideoAttachment(
+    private val id: Int,
+    private val ownerId: Int,
+    private val title: String,
+    private val description: String,
+    private val duration: Int,
+    override val type: String = "Video"
+) : Attachment {
+    override fun toString(): String {
+
+        return "$type $id $ownerId $title $description $duration"
+    }
+}
+
+class AudioAttachment(
+    private val id: Int,
+    private val ownerId: Int,
+    private val artist: String,
+    private val title: String,
+    private val duration: Int,
+    override val type: String = "Audio"
+) : Attachment {
+    override fun toString(): String {
+
+        return "$type $id $ownerId $artist $title $duration"
+    }
+}
+
+class PhotoAttachment(
+    private val id: Int,
+    private val albumId: Int,
+    private val ownerId: Int,
+    private val userId: Int,
+    private val text: String,
+    private val date: Int,
+    override val type: String = "Photo"
+) : Attachment {
+    override fun toString(): String {
+
+        return "$type $id $albumId $ownerId $userId $text $date"
+    }
+}
+
+class LinkAttachment(
+    private val url: String,
+    private val title: String,
+    private val caption: String,
+    private val description: String,
+    private val previewPage: String,
+    override val type: String = "Link"
+) : Attachment {
+    override fun toString(): String {
+
+        return "$type $url $title $caption $description $previewPage"
+    }
+}
+
+class DocAttachment(
+    private val id: Int,
+    private val ownerId: Int,
+    private val title: String,
+    private val size: Int,
+    private val ext: String,
+    private val date: Int,
+    override val type: String = "Doc"
+) : Attachment {
+    override fun toString(): String {
+
+        return "$type $id $ownerId $title $size $ext $date"
+    }
 }
 
 class Views(private val count: Int) {
@@ -42,7 +117,8 @@ object WallService {
                     copyright = post.copyright,
                     friendsOnly = post.friendsOnly,
                     views = post.views,
-                    copyHistory = post.copyHistory
+                    copyHistory = post.copyHistory,
+                    attachments = post.attachments
                 )
                 return true
             }
@@ -51,7 +127,7 @@ object WallService {
     }
 
     override fun toString(): String {
-        var str: String = ""
+        var str = ""
         for (post in posts) {
             str += post.toString() + "\n"
         }
@@ -60,15 +136,35 @@ object WallService {
 }
 
 fun main() {
-    WallService.add(Post(2, 3, 4, 5, 6, "5" , "6", false, Views(20), arrayOf(110, 220)))
-    WallService.add(Post(8, 9, 10, 7, 8, "11", "12", false, Views(10), arrayOf(115, 225, 338)))
-    WallService.add(Post(18, 9, 10, 7, 8, "11", "12", false, Views(10), arrayOf(115, 225, 338)))
-    WallService.add(Post(11, null, null, null, null, "111", null, true, Views(13), null))
+    var attachment: Array<Attachment> = emptyArray()
+    attachment += VideoAttachment(1, 2, "Vij", "good", 10)
+    attachment += AudioAttachment(2, 2, "Sting", "Song", 15)
+    WallService.add(Post(2, 3, 4, 5, 6, "5", "6", false, Views(20), arrayOf(110, 220), attachment))
+
+    attachment = emptyArray()
+    attachment += VideoAttachment(1, 2, "Blood", "not good", 10)
+    attachment += AudioAttachment(2, 2, "Coj", "Song2", 15)
+    WallService.add(Post(8, 9, 10, 7, 8, "11", "12", false, Views(10), arrayOf(115, 225, 338), attachment))
+
+    attachment = emptyArray()
+    attachment += LinkAttachment("aks-com.ru", "Electricdesign", "caption", "description", "previewPage")
+    attachment += DocAttachment(5, 6, "Book", 20, "doc", 123)
+    WallService.add(Post(18, 9, 10, 7, 8, "11", "12", false, Views(10), arrayOf(115, 225, 338), attachment))
+
+    attachment = emptyArray()
+    attachment += PhotoAttachment(1, 2, 7, 20, "Photo of forest", 120)
+    WallService.add(Post(11, null, null, null, null, "111", null, true, Views(13), null, attachment))
+
     print(WallService.toString())
     println()
-    WallService.update(Post(2222, 3222, 4, 5, 6, "5", "6", false, Views(20), arrayOf(110, 220), 2))
-    WallService.update(Post(2222, 3222, 4, 5, 6, "5", "6", false, Views(20), arrayOf(110, 220), 5))
-    WallService.update(Post(2222, 3222, 4, 5, 6, "5", "6", false, Views(20), arrayOf(110, 220), 4))
+
+    WallService.update(Post(2222, 3222, 4, 5, 6, "5", "6", false, Views(20), arrayOf(110, 220), null, 2))
+    WallService.update(Post(2222, 3222, 4, 5, 6, "5", "6", false, Views(20), arrayOf(110, 220), attachment, 5))
+
+    attachment = emptyArray()
+    attachment += VideoAttachment(1, 2, "Blood", "not good", 10)
+    attachment += AudioAttachment(2, 2, "Coj", "Song2", 15)
+    WallService.update(Post(2222, 3222, 4, 5, 6, "5", "6", false, Views(20), arrayOf(110, 220), attachment, 4))
     print(WallService.toString())
 
 }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -6,21 +6,75 @@ class WallServiceTest {
 
     @Test
     fun add() {
-        val id = WallService.add(Post(2, 3, 4, 5, 6, "5", "6", false, Views(20), arrayOf(110, 220))).id
+        val id = WallService.add(
+            Post(
+                2, 3, 4, 5, 6, "5", "6", false, Views(20), arrayOf(110, 220),
+                arrayOf(PhotoAttachment(1, 2, 7, 20, "Photo of forest", 120))
+            )
+        ).id
         assertTrue(id > 0)
     }
 
     @Test
     fun updateTrue() {
-        WallService.add(Post(8, 9, 10, 7, 8, "11", "12", false, Views(10), arrayOf(115, 225, 338)))
-        val result = WallService.update(Post(2222, 3222, 4, 5, 6, "5", "6", false, Views(20), arrayOf(110, 220), 1))
+        WallService.add(
+            Post(
+                8, 9, 10, 7, 8, "11", "12", false, Views(10), arrayOf(115, 225, 338),
+                arrayOf(PhotoAttachment(1, 2, 7, 20, "Photo of forest", 120))
+            )
+        )
+        val result = WallService.update(
+            Post(
+                2222,
+                3222,
+                4,
+                5,
+                6,
+                "5",
+                "6",
+                false,
+                Views(20),
+                arrayOf(110, 220),
+                arrayOf(PhotoAttachment(1, 2, 7, 20, "Photo of forest", 120)),
+                1
+            )
+        )
         assertTrue(result)
     }
 
     @Test
     fun updateFalse() {
-        WallService.add(Post(8, 9, 10, 7, 8, "11", "12", false, Views(10), arrayOf(115, 225, 338)))
-        val result = WallService.update(Post(2222, 3222, 4, 5, 6, "5", "6", false, Views(20), arrayOf(110, 220), 2))
+        WallService.add(
+            Post(
+                8,
+                9,
+                10,
+                7,
+                8,
+                "11",
+                "12",
+                false,
+                Views(10),
+                arrayOf(115, 225, 338),
+                arrayOf(PhotoAttachment(1, 2, 7, 20, "Photo of forest", 120))
+            )
+        )
+        val result = WallService.update(
+            Post(
+                2222,
+                3222,
+                4,
+                5,
+                6,
+                "5",
+                "6",
+                false,
+                Views(20),
+                arrayOf(110, 220),
+                arrayOf(PhotoAttachment(1, 2, 7, 20, "Photo of forest", 120)),
+                2
+            )
+        )
         assertFalse(result)
     }
 }
